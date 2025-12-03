decdcae4f7e8ef3cc7d0551b8a84306416041ad9
Pavelhaj93 <pavelhajduch93@gmail.com>
2025-12-03 22:52:19 +0700

    feat: test contact section

--- frontend/lib/nodemailer.ts
+++ frontend/lib/nodemailer.ts
@@ -4,12 +4,15 @@ export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'mailproxy.webglobe.com',
   port: Number(process.env.SMTP_PORT) || 587,
   secure: false,
+  requireTLS: true,
   auth: {
-    user: process.env.SMTP_USER || 'info@hledammotory.cz',
-    pass: process.env.SMTP_PASSWORD || '',
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
   },
+  authMethod: 'PLAIN',
   tls: {
     rejectUnauthorized: false,
+    ciphers: 'SSLv3',
   },
   debug: true,
   logger: true,
